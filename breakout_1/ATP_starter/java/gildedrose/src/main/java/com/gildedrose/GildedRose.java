1338aa7e46c7d4b605c98510c8f69f76081ef271
Kade Coates <kade.coates@m3as.com>
2025-12-16 16:43:31 -0500

    kade's changes from refactor mobbing

--- breakout_1/ATP_starter/java/gildedrose/src/main/java/com/gildedrose/GildedRose.java
+++ breakout_1/ATP_starter/java/gildedrose/src/main/java/com/gildedrose/GildedRose.java
@@ -1,6 +1,11 @@
 package com.gildedrose;
 
 class GildedRose {
+    private static final int _sellByThreshold = 0;
+    private static final int _lastChanceWindow = 6;
+    private static final int _premiumWindow = 11;
+    private static final int _minQuality = 0;
+    private static final int _maxQuality = 50;
     Item[] items;
 
     public GildedRose(Item[] items) {
@@ -8,51 +13,62 @@ class GildedRose {
     }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
+        for (int i = 0; i < items.length; i++) { 
+            // Decrease quality by 1 of a non-cheese and non-backstage pass item
             if (!items[i].name.equals("Aged Brie")
                     && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
+                if (items[i].quality > _minQuality) {
+                    // Decrease quality by 1
                     if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
+                        items[i].quality = items[i].quality - 1; 
                     }
                 }
+            // Increase quality variably based on selling days left
             } else {
-                if (items[i].quality < 50) {
+                // Increase quality by 1
+                if (items[i].quality < _maxQuality) {
                     items[i].quality = items[i].quality + 1;
 
                     if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
+                        // Increase quality by an additional 1 if there are 10 days or less
+                        if (items[i].sellIn < _premiumWindow) {
+                            if (items[i].quality < _maxQuality) {
                                 items[i].quality = items[i].quality + 1;
                             }
                         }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
+                        // Increase quality by an additional 1 if there are 5 days or less
+                        if (items[i].sellIn < _lastChanceWindow) {
+                            if (items[i].quality < _maxQuality) {
                                 items[i].quality = items[i].quality + 1;
                             }
                         }
                     }
                 }
             }
-
+            // Decrease sellIn by 1 for all items except Sulfuras
             if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
                 items[i].sellIn = items[i].sellIn - 1;
             }
-
-            if (items[i].sellIn < 0) {
+            
+            // Once the sell by date has passed
+            if (items[i].sellIn < _sellByThreshold) {
+                // Decrease quality by 1 of a non-cheese and non-backstage pass item
                 if (!items[i].name.equals("Aged Brie")) {
                     if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
+                        if (items[i].quality > _minQuality) {
                             if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
                                 items[i].quality = items[i].quality - 1;
                             }
                         }
+                    // Quality drops to 0 after the concert
                     } else {
                         items[i].quality = items[i].quality - items[i].quality;
                     }
-                } else {
-                    if (items[i].quality < 50) {
+
+                }
+                // Increase quality by 1 for cheese after sell by date
+                else {
+                    if (items[i].quality < _maxQuality) {
                         items[i].quality = items[i].quality + 1;
                     }
                 }
--- breakout_1/ATP_starter/java/gildedrose/src/test/java/com/gildedrose/GildedRoseTest.java
+++ breakout_1/ATP_starter/java/gildedrose/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -2,7 +2,6 @@ package com.gildedrose;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Assertions;
