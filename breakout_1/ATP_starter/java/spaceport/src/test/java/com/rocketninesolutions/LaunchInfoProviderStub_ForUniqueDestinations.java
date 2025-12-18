3d6af0f53106b18e7ff00002cea352583cb32a3c
Paul Moore <pdmoore@gmail.com>
2025-12-17 16:07:12 -0800

    preferred implementation. Don't expect caller to provide a mutable list, make a mutable copy internally.

--- breakout_1/ATP_starter/java/spaceport/src/test/java/com/rocketninesolutions/LaunchInfoProviderStub_ForUniqueDestinations.java
+++ breakout_1/ATP_starter/java/spaceport/src/test/java/com/rocketninesolutions/LaunchInfoProviderStub_ForUniqueDestinations.java
@@ -1,17 +1,19 @@
 package com.rocketninesolutions;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class LaunchInfoProviderStub_ForUniqueDestinations implements ISpacelineLaunchInfoProvider {
 
     @Override
     public List<LaunchInfo> getCurrentLaunches() {
+        return List.of(
+                createLaunchInfo("Moon"),
+                createLaunchInfo("Mars"));
 
-        List<LaunchInfo> launches = new ArrayList<>();
-        launches.add(createLaunchInfo("Moon"));
-        launches.add(createLaunchInfo("Mars"));
-        return launches;
+//        List<LaunchInfo> launches = new ArrayList<>();
+//        launches.add(createLaunchInfo("Moon"));
+//        launches.add(createLaunchInfo("Mars"));
+//        return launches;
     }
 
     private LaunchInfo createLaunchInfo(String destination) {
